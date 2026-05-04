8918d9821c6c79f686a4a6a4b06f89d1c5d3515e
CrypticFigure <sfpostigo2014@gmail.com>
2026-05-04 13:54:20 -0400

    Create branchExample.py
    I created a file to demonstrate github branches

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name:            branchExample.py
+Author(s):       Sebastian Postigo, Tudor Barcan
+Description:     Demonstrate how to use GitHub branches
+Date:            5/4/26
+"""
+
+counter = 0
+
+while (counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
