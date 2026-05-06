a704c664835d5a3c864af3eca3bdc46f3c07fff4
CrypticFigure <sfpostigo2014@gmail.com>
2026-05-05 23:52:19 -0400

    Created new VEX Project
    Used VEX Robotics Extension to create new VEX V5 Python project
    - created extensions.json
    - created vex_project_settings.json
    - created main.py

---
+++ postigo-vex/src/main.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Admin                                                        #
+# 	Created:      5/4/2026, 2:15:00 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+
+
+        
